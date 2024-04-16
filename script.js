3e489d9f5a30bcd97d3436e045c2333a3317a8ab
Eden Urias <64157425+AvacadoWizard120@users.noreply.github.com>
2024-04-16 11:22:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const gridContainer = document.getElementById('gridContainer');
 
-const imagesDir = 'maps/'; // Directory where your images are stored
-
-fetch(imagesDir)
-    .then(response => response.text())
-    .then(text => {
-        const parser = new DOMParser();
-        const htmlDocument = parser.parseFromString(text, 'text/html');
-        const imageNames = Array.from(htmlDocument.querySelectorAll('a'))
-            .map(a => a.getAttribute('href'))
-            .filter(href => href.match(/\.(avif|jpg|webp)$/));
-        
-        imageNames.forEach((imageName, index) => {
-            const imageUrl = `${imagesDir}${imageName}`;
+fetch('maps.json')
+    .then(response => response.json())
+    .then(data => {
+        data.maps.forEach((mapName, index) => {
+            const imageUrl = `maps/${mapName}`;
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
             const image = document.createElement('img');
             image.src = imageUrl;
-            image.alt = imageName;
+            image.alt = mapName;
             gridItem.appendChild(image);
             gridItem.addEventListener('click', () => {
                 gridContainer.removeChild(gridItem);
@@ -26,5 +18,5 @@ fetch(imagesDir)
         });
     })
     .catch(error => {
-        console.error('Error fetching images:', error);
+        console.error('Error fetching map data:', error);
     });
